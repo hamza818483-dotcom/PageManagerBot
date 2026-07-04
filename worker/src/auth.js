baf4413f5f80174365e55b53416bc29270b4cee1
hamza818483-dotcom <hamza818483@gmail.com>
2026-07-04 06:33:54 +0000

    fix: remove unapproved pages_messaging scope, use business_management

--- worker/src/auth.js
+++ worker/src/auth.js
@@ -8,7 +8,7 @@ export async function handleAuthLogin(request, env) {
     'pages_read_engagement',
     'pages_manage_metadata',
     'pages_manage_engagement',
-    'pages_messaging'
+    'business_management'
   ].join(',');
   const authUrl = `https://www.facebook.com/${env.FB_GRAPH_VERSION}/dialog/oauth?client_id=${env.FB_APP_ID}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&response_type=code`;
   return Response.redirect(authUrl, 302);
